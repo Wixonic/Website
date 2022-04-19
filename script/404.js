5536c8d77ece04774414d2119d04d53e2818c007
Wixonic <public.wixonic@icloud.com>
2022-04-19 19:31:24 +0200

    Add files via upload

--- script/404.js
+++ script/404.js
@@ -1,14 +1,53 @@
 onload = () => {
 	const xhr = new XMLHttpRequest();
 	
-	xhr.open("GET","https://wixonic.github.io/sitelinks.json",false);
+	xhr.open("GET","/sitelinks.json",false);
+	
+	xhr.responseType = "json";
 	
 	xhr.onload = () => {
 		window.links = xhr.response;
 		
-		const input = document.getElementById("search");
-		input.value = window.location.pathname;
+		if (typeof(links) != "object") {
+			links = {};
+		}
+		
+		window.input = document.getElementById("search");
+		window.output = document.getElementById("results");
+		
+		input.value = window.location.pathname.replace("/","").split("/").join(" ");
+		input.addEventListener("input",search);
+		search();
 	};
 	
 	xhr.send();
+};
+
+const validchars = (txt) => txt.split(" ").join("").split("	").join("").toLowerCase();
+
+const search = () => {
+	const globalcheck = validchars(input.value).length <= 0;
+	
+	const value = input.value.split(" ");
+	
+	output.innerHTML = "";
+	
+	for (let name in links) {
+		const link = links[name];
+		
+		for (let x = 0; x < value.length; ++x) {
+			const v = validchars(value[x]);
+			
+			const check = v.length > 0 && (validchars(name).indexOf(v) != -1) || (validchars(link).indexOf(v) != -1);
+			
+			if (globalcheck || check) {
+				output.innerHTML += `<a class="result" href="${link}"><name>${name}</name><ref>${link}</ref></a>`;
+				break;
+			}
+		}
+	}
+	
+	if (output.innerHTML == "") {
+		output.innerHTML = `<a class="result" href="/"><name>No result</name><ref>Go to the Homepage</ref></a>`;
+	}
 };
