df7b768f7ada73f0508f43a38e20284674383bd2
Wixonic <public.wixonic@icloud.com>
2022-07-21 09:05:44 +0200

    Update WebRTCTest.js

--- temporary/WebRTCTest.js
+++ temporary/WebRTCTest.js
@@ -14,8 +14,11 @@ onload = () => {
 	
 	window.app = initializeApp({
 		apiKey: "AIzaSyBOK-u-u6mx3ZdwTXBTuY61qox34baxkvs",
+		authDomain: "wixonic-webrtc-test.firebaseapp.com",
 		databaseURL: "https://wixonic-webrtc-test-default-rtdb.europe-west1.firebasedatabase.app",
 		projectId: "wixonic-webrtc-test",
+		storageBucket: "wixonic-webrtc-test.appspot.com",
+		messagingSenderId: "526261810557",
 		appId: "1:526261810557:web:6a49a7291b0a5556b4c814"
 	});
 	
