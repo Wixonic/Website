565eb29356347950430d6e25710dabefbccdbce5
Wixonic <public.wixonic@icloud.com>
2023-09-03 00:07:24 +0200

    Update

--- script/404.js
+++ script/404.js
@@ -11,30 +11,30 @@ onload = () => {
 		appId: "1:595027315977:web:6f8db9ef2aa44614b457d8",
 		measurementId: "G-QVK9NE4M07"
 	});
-	
+
 	window.Analytics = getAnalytics(App);
 
 	const xhr = new XMLHttpRequest();
-	
-	xhr.open("GET","https://wixonic.fr/sitelinks.json",true);
-	
+
+	xhr.open("GET", "https://wixonic.fr/sitelinks.json", true);
+
 	xhr.responseType = "json";
-	
+
 	xhr.onload = () => {
 		window.links = xhr.response;
-		
-		if (typeof(links) != "object") {
+
+		if (typeof (links) != "object") {
 			links = {};
 		}
-		
+
 		window.input = document.getElementById("search");
 		window.output = document.getElementById("results");
-		
-		input.value = window.location.pathname.replace("/","").split("/").join(" ");
-		input.addEventListener("input",search);
+
+		input.value = window.location.pathname.replace("/", "").split("/").join(" ");
+		input.addEventListener("input", search);
 		search();
 	};
-	
+
 	xhr.send();
 };
 
@@ -42,26 +42,26 @@ const validchars = (txt) => txt.split(" ").join("").split("	").join("").toLowerC
 
 const search = () => {
 	const globalcheck = validchars(input.value).length <= 0;
-	
+
 	const value = input.value.split(" ");
-	
+
 	output.innerHTML = "";
-	
+
 	for (let name in links) {
 		const link = links[name];
-		
+
 		for (let x = 0; x < value.length; ++x) {
 			const v = validchars(value[x]);
-			
+
 			const check = v.length > 0 && (validchars(name).indexOf(v) != -1) || (validchars(link).indexOf(v) != -1);
-			
+
 			if (globalcheck || check) {
 				output.innerHTML += `<a class="result" href="${link}"><name>${name}</name><ref>${link}</ref></a>`;
 				break;
 			}
 		}
 	}
-	
+
 	if (output.innerHTML == "") {
 		output.innerHTML = `<a class="result" href="/"><name>No result</name><ref>Go to the Homepage</ref></a>`;
 	}
--- script/index.js
+++ script/index.js
@@ -14,7 +14,7 @@ window.App = initializeApp({
 window.Analytics = getAnalytics(App);
 
 const xhr = new XMLHttpRequest();
-xhr.open("GET","https://website-wixonic-default-rtdb.europe-west1.firebasedatabase.app/news.json",true);
+xhr.open("GET", "https://website-wixonic-default-rtdb.europe-west1.firebasedatabase.app/news.json", true);
 xhr.responseType = "json";
 
 xhr.onload = () => {
@@ -22,11 +22,11 @@ xhr.onload = () => {
 		const timestamps = Object.keys(xhr.response);
 		timestamps.sort();
 		document.getElementById("news").innerHTML = "";
-		timestamps.forEach((timestamp,i) => {
+		timestamps.forEach((timestamp, i) => {
 			const data = xhr.response[timestamp];
 
 			const item = document.createElement("item");
-			item.addEventListener("click",() => window.open(data.link,"_blank"));
+			item.addEventListener("click", () => window.open(data.link, "_blank"));
 			item.innerHTML = `<i class="${data.icon}"></i><b>${data.title}</b><p>${data.description}</p>`;
 			document.getElementById("news").prepend(item);
 		});
@@ -39,35 +39,35 @@ xhr.onload = () => {
 			"title": "Instagram",
 			"icon": "fa-brands fa-instagram",
 			"link": "https://www.instagram.com/wixonic12"
-		},{
+		}, {
 			"title": "GitHub",
 			"icon": "fa-brands fa-github",
 			"link": "https://github.com/Wixonic"
-		},{
+		}, {
 			"title": "Discord",
 			"icon": "fa-brands fa-discord",
 			"link": "https://discord.gg/DSkSPTUP95"
-		},{
+		}, {
 			"title": "YouTube",
 			"icon": "fa-brands fa-youtube",
 			"link": "https://youtube.com/@wixonic"
-		},{
+		}, {
 			"title": "Facebook",
 			"icon": "fa-brands fa-facebook",
 			"link": "https://www.facebook.com/Wixonic/"
-		},{
+		}, {
 			"title": "Twitch",
 			"icon": "fa-brands fa-twitch",
 			"link": "https://www.twitch.tv/Wixonic"
-		},{
+		}, {
 			"title": "Twitter",
 			"icon": "fa-brands fa-twitter",
 			"link": "https://twitter.com/Wixonic"
-		},{
+		}, {
 			"title": "Stack Overflow",
 			"icon": "fa-brands fa-stack-overflow",
 			"link": "https://stackoverflow.com/users/18716068/wixonic"
-		},{
+		}, {
 			"title": "Reddit",
 			"icon": "fa-brands fa-reddit-alien",
 			"link": "https://www.reddit.com/user/Wixonic12"
@@ -78,11 +78,11 @@ xhr.onload = () => {
 
 	for (let link of links) {
 		const item = document.createElement("item");
-		item.addEventListener("click",() => window.open(link.link,"_blank"));
+		item.addEventListener("click", () => window.open(link.link, "_blank"));
 		item.innerHTML = `<i class="${link.icon}"></i><p>${link.title}</p>`;
 		document.getElementById("links").append(item);
 	}
-	
+
 	document.getElementById("contact").innerHTML = "";
 
 	const contact = [
@@ -90,7 +90,7 @@ xhr.onload = () => {
 			"title": "Instagram",
 			"icon": "fa-brands fa-instagram",
 			"link": "https://www.instagram.com/wixonic12"
-		},{
+		}, {
 			"title": "Email",
 			"icon": "fa-duotone fa-envelope",
 			"link": "mailto:contact@wixonic.fr"
@@ -99,7 +99,7 @@ xhr.onload = () => {
 
 	for (let link of contact) {
 		const item = document.createElement("item");
-		item.addEventListener("click",() => window.open(link.link,"_blank"));
+		item.addEventListener("click", () => window.open(link.link, "_blank"));
 		item.innerHTML = `<i class="${link.icon}"></i><p>${link.title}</p>`;
 		document.getElementById("contact").append(item);
 	}
