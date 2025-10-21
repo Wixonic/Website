e05a2e3d69beb5c1a9af30cbe8b982af5f1d2c50
Wixonic <contact@wixonic.fr>
2025-10-21 10:42:22 +0200

    Cleaning

--- websites/root/privacy/index.js
+++
@@ -1,9 +0,0 @@
-import { init } from "/lib/main.js";
-import { RichLink } from "/lib/rich.js";
-
-addEventListener("DOMContentLoaded", async () => {
-	const sleepingRichLinks = document.getElementsByClassName("sleepingRichLink");
-	for (let x = 0; x < sleepingRichLinks.length; ++x) RichLink(new URL(sleepingRichLinks[x].href), sleepingRichLinks[x]);
-
-	await init();
-});
