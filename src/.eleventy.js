7ac9a08b27b16cfcc1c1cd70dadd5e4f2ef19aa8
Wixonic <contact@wixonic.fr>
2025-11-12 18:59:43 +0100

    Update

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -8,8 +8,24 @@ export default (config) => {
 	config.addPassthroughCopy("./websites/**/*.css");
 	config.addPassthroughCopy("./websites/**/*.js");
 
-	config.addFilter("cleanPathPrefix", (path) => {
-		if (!path) return "/";
-		return path.replace(/^\/websites\/[^\/]+\/?/, "/");
+	config.addFilter("cleanPath", (filePath) => {
+		if (!filePath) return "https://wixonic.fr";
+
+		try {
+			const target = filePath.match(/\/websites\/([^\/]+)/)[1];
+			const origin = {
+				"root": "https://wixonic.fr",
+				"accounts": "https://accounts.wixonic.fr",
+				"admin": "https://admin.wixonic.fr",
+				"assets": "https://assets.wixonic.fr",
+				"kcmaths": "https://kcmaths.wixonic.fr"
+			};
+			const cleanPath = filePath.replace(/\/websites\/[^\/]+\/?/, "/");
+
+			return origin[target] + cleanPath;
+		} catch (e) {
+			console.warn(`Failed to clean path for "${filePath}": `, e.message);
+			return "https://wixonic.fr";
+		}
 	});
 };
