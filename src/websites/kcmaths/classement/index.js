00aa0acc1999b1bb56cc42b1f4b3158bd045f49f
Wixonic <contact@wixonic.fr>
2025-11-21 13:45:22 +0100

    Update KCMaths squared

--- src/websites/kcmaths/classement/index.js
+++ src/websites/kcmaths/classement/index.js
@@ -3,16 +3,138 @@ import { init } from "/lib/main.js";
 import { path, updateURL } from "/lib/script/path.js";
 import request from "/lib/script/request.js";
 
+import { findRankFor, formatLeaderboard, formatRank, sortLeaderboard } from "/utils.js";
+
 addEventListener("DOMContentLoaded", async () => {
 	await init();
 
+	const date = document.querySelector(".input.date");
+
+	/** @type {Date[]} */
 	let entries = await request("GET", new URL("/kcmaths/entries/", path.server), "json", "application/json", null, 600);
 
 	if ([200, 204].includes(entries.status)) entries = entries.response;
 	else entries = [];
 
-	document.querySelector(".input.date").setAttribute("dates", entries.join(", "));
+	date.setAttribute("dates", entries.join(", "));
 
-	for (let i = 0; i < entries.length; ++i) entries[i] = new Date(entries[i]);
+	for (let i = 0; i < entries.length; ++i) {
+		entries[i] = new Date(entries[i]);
+		entries[i].setUTCHours(0, 0, 0, 0);
+	}
+
 	entries = entries.sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
+
+	/**
+	 * @param {Number} id
+	 * @param {"ratio" | "bank" | "entries"} category
+	 */
+	const loadLeaderboard = async (id, category) => {
+		const container = document.querySelector(".classement");
+
+		if (id == -1) {
+			container.innerHTML = "Aucune donnée pour cette date.";
+		} else {
+			container.innerHTML = "";
+			const date = entries[id];
+			const previous = id > 0 ? entries[id - 1] : [];
+
+			let leaderboard = await request("GET", new URL(`/kcmaths/leaderboard/?date=${date.getUTCFullYear()}-${String(date.getUTCMonth() + 1).padStart(2, "0")}-${String(date.getUTCDate()).padStart(2, "0")}`, path.server), "json", "application/json", null, 600);
+
+			if ([200].includes(leaderboard.status)) leaderboard = formatLeaderboard(leaderboard.response);
+			else leaderboard = {};
+
+			let previousLeaderboard = await request("GET", new URL(`/kcmaths/leaderboard/?date=${previous.getUTCFullYear()}-${String(previous.getUTCMonth() + 1).padStart(2, "0")}-${String(previous.getUTCDate()).padStart(2, "0")}`, path.server), "json", "application/json", null, 600);
+
+			if ([200].includes(previousLeaderboard.status)) previousLeaderboard = formatLeaderboard(previousLeaderboard.response);
+			else previousLeaderboard = {};
+
+			const sortedLeaderboard = sortLeaderboard(leaderboard, category);
+			for (const id in sortedLeaderboard) {
+				const rank = findRankFor(leaderboard, id, category);
+				const previousRank = findRankFor(previousLeaderboard, id, category);
+
+				const entry = leaderboard[id];
+				const previousEntry = previousLeaderboard[id];
+
+				const element = document.createElement("div");
+				element.classList.add("membre");
+
+				{
+					const rankEl = document.createElement("div");
+					rankEl.classList.add("rang");
+					rankEl.innerHTML = formatRank(rank);
+					element.append(rankEl);
+
+					const deltaEl = document.createElement("div");
+					deltaEl.classList.add("delta");
+					const delta = previousRank - rank;
+					if (delta != 0) {
+						deltaEl.innerHTML = `${delta > 0 ? "+" : ""}${delta}`;
+						deltaEl.classList.add(delta > 0 ? "positif" : "negatif");
+					}
+					element.append(rankEl);
+
+					const nameEl = document.createElement("div");
+					nameEl.classList.add("nom");
+					nameEl.innerHTML = id;
+					element.append(nameEl);
+
+					const valueEl = document.createElement("div");
+					valueEl.classList.add("valeur");
+					element.append(valueEl);
+
+					const deltaValueEl = document.createElement("div");
+					deltaValueEl.classList.add("delta-valeur");
+					element.append(deltaValueEl);
+
+					const detailsEl = document.createElement("div");
+					detailsEl.classList.add("details");
+					element.append(detailsEl);
+
+					switch (category) {
+						case "ratio":
+							valueEl.innerHTML = entry.ratio > 0 ? `${Number((entry.ratio * 100).toFixed(2))}%` : "--";
+							const deltaRatio = entry.ratio > 0 ? entry.ratio - previousEntry.ratio : 0;
+							if (deltaRatio != 0) {
+								deltaValueEl.innerHTML = `${deltaRatio > 0 ? "+" : ""}${Number((deltaRatio * 100).toFixed(2))}%`;
+								deltaValueEl.classList.add(deltaRatio > 0 ? "positif" : "negatif");
+							}
+							break;
+
+						case "bank":
+							valueEl.innerHTML = entry.kcCoins;
+							const deltaKCC = entry.kcCoins - previousEntry.kcCoins;
+							if (deltaKCC != 0) {
+								deltaValueEl.innerHTML = `${deltaKCC > 0 ? "+" : ""}${deltaKCC}`;
+								deltaValueEl.classList.add(deltaKCC > 0 ? "positif" : "negatif");
+							}
+							break;
+
+						case "entries":
+							valueEl.innerHTML = entry.entries;
+							const deltaEntries = entry.entries - previousEntry.entries;
+							if (deltaEntries != 0) {
+								deltaValueEl.innerHTML = `${deltaEntries > 0 ? "+" : ""}${deltaEntries}`;
+								deltaValueEl.classList.add(deltaEntries > 0 ? "positif" : "negatif");
+							}
+							break;
+					}
+				}
+
+				container.append(element);
+			}
+		}
+	};
+
+	const params = new URLSearchParams(location.search);
+	const category = ["ratio", "bank", "entries"].includes(params.get("category")) ? params.get("category") : "ratio";
+	if (params.get("date")) {
+		const selectedDate = new Date(params.get("date"));
+		selectedDate.setUTCHours(0, 0, 0, 0);
+		date.setAttribute("date", `${selectedDate.getUTCFullYear()}-${String(selectedDate.getUTCMonth() + 1).padStart(2, "0")}-${String(selectedDate.getUTCDate()).padStart(2, "0")}`);
+
+		const entry = entries.findIndex((value) => `${selectedDate.getUTCFullYear()}-${String(selectedDate.getUTCMonth() + 1).padStart(2, "0")}-${String(selectedDate.getUTCDate()).padStart(2, "0")}` == `${value.getUTCFullYear()}-${String(value.getUTCMonth() + 1).padStart(2, "0")}-${String(value.getUTCDate()).padStart(2, "0")}`) ?? -1;
+		await loadLeaderboard(entry, category);
+	} else await loadLeaderboard(entries.length - 1, category);
 });
--- src/websites/kcmaths/utils.js
+++ src/websites/kcmaths/utils.js
@@ -4,23 +4,60 @@ const formatRank = (rank) => {
 	else return rank + "e";
 };
 
-const findRankFor = (leaderboard, id) => {
+const findRankFor = (leaderboard, id, category) => {
+	const sortedLeaderboard = sortLeaderboard(leaderboard, category);
 	let previousEntry = { id: 0, value: 0 };
 
-	if (Object.keys(leaderboard).indexOf(id) == -1) return -1;
-	if (id.endsWith("Corbineau")) return 0;
+	if (Object.keys(sortedLeaderboard).indexOf(id) == -1) return -1;
 
 	let currentId = 0;
-	for (const name in leaderboard) {
-		++currentId;
-		if (previousEntry.value != leaderboard[name]) previousEntry = { id: currentId, value: leaderboard[name] };
+	for (const name in sortedLeaderboard) {
+		if (!name.endsWith("Corbineau")) ++currentId;
+		const value = sortedLeaderboard[name][{
+			"ratio": "ratio",
+			"bank": "kcCoins",
+			"entries": "entries"
+		}[category]];
+		if (previousEntry.value != value) previousEntry = { id: currentId, value };
 		if (name == id) break;
 	}
 
 	return previousEntry.id;
 };
 
+const formatLeaderboard = (leaderboard) => {
+	for (const id in leaderboard) leaderboard[id].ratio = leaderboard[id].entries > 0 ? -1 : leaderboard[id].victories / leaderboard[id].entries;
+	return leaderboard;
+};
+
+const sortLeaderboard = (leaderboard, category) => {
+	const values = Object.values(leaderboard);
+	values.sort((a, b) => {
+		switch (category) {
+			case "ratio":
+				if (a.percent == b.percent) return b.entries - a.entries;
+				else return b.percent - a.percent;
+
+			case "bank":
+				if (a.kcCoins == b.kcCoins) return b.percent - a.percent;
+				else return b.kcCoins - a.kcCoins;
+
+			case "entries":
+				if (a.entries == b.entries) return b.percent - a.percent;
+				else return b.entries - a.entries;
+
+			default:
+				return 0;
+		};
+	});
+	const sortedLeaderboard = {};
+	for (const value of values) sortedLeaderboard[`${value.firstName} ${value.lastName}`] = value;
+	return sortedLeaderboard;
+};
+
 export {
 	findRankFor,
-	formatRank
+	formatLeaderboard,
+	formatRank,
+	sortLeaderboard
 };
