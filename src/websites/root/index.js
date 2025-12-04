0325b6958f6727f2145a86a1d89dffb098ad7477
Wixonic <contact@wixonic.fr>
2025-12-04 17:51:34 +0100

    Update design

--- src/websites/root/index.js
+++ src/websites/root/index.js
@@ -2,27 +2,4 @@ import { init } from "/lib/main.js";
 
 addEventListener("DOMContentLoaded", async () => {
 	await init();
-
-	const sections = document.querySelectorAll("section");
-	const separator = document.querySelector("separator");
-
-	separator.addEventListener("click", () => {
-		sections[1].scrollIntoView({
-			behavior: "smooth",
-			block: "start"
-		});
-	});
-
-	const observer = new IntersectionObserver((entries) => {
-		entries.forEach((entry) => {
-			if (entry.isIntersecting) {
-				document.body.classList.toggle("dark", entry.target.classList.contains("wixiland"));
-			}
-		});
-	}, {
-		root: null,
-		threshold: 0.5
-	});
-
-	sections.forEach((section) => observer.observe(section));
 });
