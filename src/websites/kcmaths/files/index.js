e4e01231cec5282627733a8ebe7f818cf9fa4bde
Wixonic <contact@wixonic.fr>
2025-12-15 17:17:03 +0100

    Update

--- src/websites/kcmaths/files/index.js
+++ src/websites/kcmaths/files/index.js
@@ -3,6 +3,6 @@ import { init } from "/lib/main.js";
 addEventListener("DOMContentLoaded", async () => {
 	await init();
 
+	document.querySelector(".extra-header .overview").addEventListener("click", () => location.href = path.kcmaths);
 	document.querySelector(".extra-header .leaderboard").addEventListener("click", () => location.href = new URL("/leaderboard/", path.kcmaths));
-	document.querySelector(".extra-header .fiels").addEventListener("click", () => location.href = new URL("/files/", path.kcmaths));
 });
