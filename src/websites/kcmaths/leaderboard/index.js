a5e083b0ecb2211bda699c32ecc1f422049e6092
Wixonic <contact@wixonic.fr>
2025-12-17 09:39:35 +0100

    feat: Add loading animations and improve asynchronous data fetching for the leaderboard.

--- src/websites/kcmaths/leaderboard/index.js
+++ src/websites/kcmaths/leaderboard/index.js
@@ -18,245 +18,255 @@ addEventListener("DOMContentLoaded", async () => {
 	const victoriesButton = document.querySelector(".category#victories");
 
 	const factsContainer = document.querySelector(".facts");
+	const container = document.querySelector("#leaderboard");
+	const leaderboardTitle = document.querySelector("#title");
 
 	const lastUpdated = document.querySelector(".last-updated");
 
-	const lastUpdate = await request("GET", new URL("/kcmaths/lastUpdate/", path.server), "text", "plain/text", null, -1);
-	if (lastUpdate.status == 200) lastUpdated.innerHTML = `Mis à jour le ${new Date(lastUpdate.response).toLocaleString("fr-FR", {
-		weekday: "long",
-		day: "numeric",
-		month: "long",
-		hour: "numeric",
-		minute: "numeric"
-	})}`;
-	else lastUpdated.innerHTML = "Server is not available at the moment.";
+	(async () => {
+		const lastUpdate = await request("GET", new URL("/kcmaths/lastUpdate/", path.server), "text", "plain/text", null, -1);
+		lastUpdated.classList.remove("loading");
+		if (lastUpdate.status == 200) lastUpdated.textContent = `Mis à jour le ${new Date(lastUpdate.response).toLocaleString("fr-FR", {
+			weekday: "long",
+			day: "numeric",
+			month: "long",
+			hour: "numeric",
+			minute: "numeric"
+		})}`;
+		else lastUpdated.textContent = "Serveur indisponible pour le moment.";
+	})();
 
 	/** @type {Date[]} */
 	let entries = await request("GET", new URL("/kcmaths/entries/", path.server), "json", "application/json", null, 600);
 
-	if ([200, 204].includes(entries.status)) entries = entries.response;
-	else entries = [];
+	if ([200, 204].includes(entries.status)) {
+		entries = entries.response;
 
-	date.setAttribute("dates", entries.join(", "));
+		date.setAttribute("dates", entries.join(", "));
 
-	for (let i = 0; i < entries.length; ++i) {
-		entries[i] = new Date(entries[i]);
-		entries[i].setUTCHours(0, 0, 0, 0);
-	}
+		for (let i = 0; i < entries.length; ++i) {
+			entries[i] = new Date(entries[i]);
+			entries[i].setUTCHours(0, 0, 0, 0);
+		}
 
-	entries = entries.sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
-
-	/**
-	 * @param {number} id
-	 * @param {"ratio" | "bank" | "entries" | "victories"} category
-	 */
-	const loadLeaderboard = async (id, category) => {
-		for (const el of [ratioButton, bankButton, entriesButton, victoriesButton]) el.removeAttribute("disabled");
-
-		document.querySelector("#title").innerHTML = {
-			"ratio": "Pourcentage de victoires",
-			"bank": "Banque",
-			"entries": "Participations",
-			"victories": "Victoires"
-		}[category];
-
-		const container = document.querySelector("#leaderboard");
+		entries = entries.sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
 
 		/**
-		 * @param {HTMLElement} element
-		 * @param {string} id
+		 * @param {number} id
+		 * @param {"ratio" | "bank" | "entries" | "victories"} category
 		 */
-		const loadDetails = async (element, id) => {
-			const details = await request("GET", new URL(`/kcmaths/details/?id=${encodeURIComponent(id)}`, path.server), "json", "application/json", null, -1);
-			element.querySelector(".details").innerHTML = JSON.stringify(details);
-		};
+		const loadLeaderboard = async (id, category) => {
+			for (const el of [ratioButton, bankButton, entriesButton, victoriesButton]) el.removeAttribute("disabled");
 
-		if (id == -1) container.innerHTML = "Aucune donnée pour cette date.";
-		else {
-			container.innerHTML = "";
-			const date = entries[id];
-			const previous = id > 0 ? entries[id - 1] : null;
+			leaderboardTitle.innerHTML = {
+				"ratio": "Pourcentage de victoires",
+				"bank": "Banque",
+				"entries": "Participations",
+				"victories": "Victoires"
+			}[category];
 
-			let leaderboard = await request("GET", new URL(`/kcmaths/leaderboard/?date=${date.getUTCFullYear()}-${String(date.getUTCMonth() + 1).padStart(2, "0")}-${String(date.getUTCDate()).padStart(2, "0")}`, path.server), "json", "application/json", null, 600);
+			/**
+			 * @param {HTMLElement} element
+			 * @param {string} id
+			 */
+			const loadDetails = async (element, id) => {
+				const el = element.querySelector(".details");
+				el.classList.add("loading");
+				// const details = await request("GET", new URL(`/kcmaths/details/?id=${encodeURIComponent(id)}`, path.server), "json", "application/json", null, -1);
+				// el.classList.remove("loading");
+				// el.innerHTML = JSON.stringify(details);
+			};
 
-			if ([200].includes(leaderboard.status)) leaderboard = formatLeaderboard(leaderboard.response);
-			else leaderboard = {};
+			if (id == -1) container.innerHTML = "Aucune donnée pour cette date.";
+			else {
+				container.innerHTML = "";
+				const date = entries[id];
+				const previous = id > 0 ? entries[id - 1] : null;
 
-			let previousLeaderboard = previous ? await request("GET", new URL(`/kcmaths/leaderboard/?date=${previous.getUTCFullYear()}-${String(previous.getUTCMonth() + 1).padStart(2, "0")}-${String(previous.getUTCDate()).padStart(2, "0")}`, path.server), "json", "application/json", null, 600) : {};
+				let [leaderboard, previousLeaderboard] = await Promise.all([
+					request("GET", new URL(`/kcmaths/leaderboard/?date=${date.getUTCFullYear()}-${String(date.getUTCMonth() + 1).padStart(2, "0")}-${String(date.getUTCDate()).padStart(2, "0")}`, path.server), "json", "application/json", null, 600)
+						.then((res) => [200].includes(res.status) ? formatLeaderboard(res.response) : {}),
 
-			if ([200].includes(previousLeaderboard.status)) previousLeaderboard = formatLeaderboard(previousLeaderboard.response);
-			else previousLeaderboard = {};
+					previous ? request("GET", new URL(`/kcmaths/leaderboard/?date=${previous.getUTCFullYear()}-${String(previous.getUTCMonth() + 1).padStart(2, "0")}-${String(previous.getUTCDate()).padStart(2, "0")}`, path.server), "json", "application/json", null, 600)
+						.then((res) => [200].includes(res.status) ? formatLeaderboard(res.response) : {}) : Promise.resolve({})
+				]);
 
-			{
-				factsContainer.classList.add("hidden");
-				factsContainer.innerHTML = `<div class="title">Faits interréssants</div>`;
-
-				const generateFact = (text, category) => {
-					factsContainer.classList.remove("hidden");
-					const el = document.createElement("div");
-					el.innerHTML = text;
-					el.classList.add("fact", category);
-					factsContainer.append(el);
-				};
-
-				for (const id in leaderboard) {
-					const entry = leaderboard[id];
-					const previousEntry = previousLeaderboard[id];
-
-					if (!previousEntry) continue;
-
-					const generateFactForCategory = (category) => {
-						const currentRank = findRankFor(leaderboard, id, category);
-						const previousRank = findRankFor(previousLeaderboard, id, category);
-						const rankDelta = previousRank - currentRank;
-
-						const categoryText = {
-							"ratio": "en pourcentage",
-							"bank": "dans la banque",
-							"entries": "en nombre de participations",
-							"victories": "en nombre de victoires"
-						};
-
-						if (rankDelta > 15) generateFact(`${id} a progressé de ${rankDelta} places ${categoryText[category]}.`, "pos");
-						else if (rankDelta < -15) generateFact(`${id} a perdu ${Math.abs(rankDelta)} places ${categoryText[category]}.`, "neg");
-					};
-
-					generateFactForCategory("ratio");
-					generateFactForCategory("bank");
-					generateFactForCategory("entries");
-					generateFactForCategory("victories");
-
-					const kccDelta = entry.kcCoins - previousEntry.kcCoins;
-
-					if (kccDelta > 20) generateFact(`${id} a gagné ${kccDelta} KCC`, "pos");
-					else if (kccDelta < -20) generateFact(`${id} a dépensé ${Math.abs(kccDelta)} KCC`, "neg");
-
-					const victoriesDelta = entry.victories - previousEntry.victories;
-					if (victoriesDelta > 2) generateFact(`${id} a gagné ${victoriesDelta} fois`, "pos");
-				}
-			}
-
-			const sortedLeaderboard = sortLeaderboard(leaderboard, category);
-			for (const id in sortedLeaderboard) {
-				const rank = findRankFor(leaderboard, id, category);
-				const previousRank = findRankFor(previousLeaderboard, id, category);
-
-				const entry = leaderboard[id];
-				const previousEntry = previousLeaderboard[id] ?? {};
-
-				const element = document.createElement("div");
-				element.classList.add("member");
-				element.setAttribute("rank", rank);
+				container.classList.remove("loading");
 
 				{
-					const rankEl = document.createElement("div");
-					rankEl.classList.add("rank");
-					rankEl.innerHTML = formatRank(rank);
-					element.append(rankEl);
+					factsContainer.classList.add("hidden");
+					factsContainer.innerHTML = `<div class="title">Faits interréssants</div>`;
 
-					const deltaEl = document.createElement("div");
-					deltaEl.classList.add("delta");
-					const delta = previousRank - rank;
-					if (delta != 0) {
-						deltaEl.innerHTML = `${delta > 0 ? "+" : ""}${delta}`;
-						deltaEl.classList.add(delta > 0 ? "pos" : "neg");
-					}
-					element.append(deltaEl);
+					const generateFact = (text, category) => {
+						factsContainer.classList.remove("hidden");
+						const el = document.createElement("div");
+						el.innerHTML = text;
+						el.classList.add("fact", category);
+						factsContainer.append(el);
+					};
 
-					const nameEl = document.createElement("div");
-					nameEl.classList.add("name");
-					nameEl.innerHTML = id;
-					element.append(nameEl);
+					for (const id in leaderboard) {
+						const entry = leaderboard[id];
+						const previousEntry = previousLeaderboard[id];
 
-					const valueEl = document.createElement("div");
-					valueEl.classList.add("value");
-					element.append(valueEl);
+						if (!previousEntry) continue;
 
-					const deltaValueEl = document.createElement("div");
-					deltaValueEl.classList.add("delta-value");
-					element.append(deltaValueEl);
+						const generateFactForCategory = (category) => {
+							const currentRank = findRankFor(leaderboard, id, category);
+							const previousRank = findRankFor(previousLeaderboard, id, category);
+							const rankDelta = previousRank - currentRank;
 
-					const detailsEl = document.createElement("div");
-					detailsEl.classList.add("details");
-					element.append(detailsEl);
+							const categoryText = {
+								"ratio": "en pourcentage",
+								"bank": "dans la banque",
+								"entries": "en nombre de participations",
+								"victories": "en nombre de victoires"
+							};
 
-					switch (category) {
-						case "ratio":
-							valueEl.innerHTML = entry.ratio > 0 ? `${Number((entry.ratio * 100).toFixed(1))}%` : "--";
-							const deltaRatio = entry.ratio > 0 ? entry.ratio - (previousEntry.ratio ?? entry.ratio) : 0;
-							if (deltaRatio != 0) {
-								deltaValueEl.innerHTML = `${deltaRatio > 0 ? "+" : ""}${Number((deltaRatio * 100).toFixed(1))}%`;
-								deltaValueEl.classList.add(deltaRatio > 0 ? "pos" : "neg");
-							}
-							break;
+							if (rankDelta > 15) generateFact(`${id} a progressé de ${rankDelta} places ${categoryText[category]}.`, "pos");
+							else if (rankDelta < -15) generateFact(`${id} a perdu ${Math.abs(rankDelta)} places ${categoryText[category]}.`, "neg");
+						};
 
-						case "bank":
-							valueEl.innerHTML = entry.kcCoins;
-							const deltaKCC = entry.kcCoins - (previousEntry.kcCoins ?? entry.kcCoins);
-							if (deltaKCC != 0) {
-								deltaValueEl.innerHTML = `${deltaKCC > 0 ? "+" : ""}${deltaKCC}`;
-								deltaValueEl.classList.add(deltaKCC > 0 ? "pos" : "neg");
-							}
-							break;
+						generateFactForCategory("ratio");
+						generateFactForCategory("bank");
+						generateFactForCategory("entries");
+						generateFactForCategory("victories");
 
-						case "entries":
-							valueEl.innerHTML = entry.entries;
-							const deltaEntries = entry.entries - (previousEntry.entries ?? entry.entries);
-							if (deltaEntries != 0) {
-								deltaValueEl.innerHTML = `${deltaEntries > 0 ? "+" : ""}${deltaEntries}`;
-								deltaValueEl.classList.add(deltaEntries > 0 ? "pos" : "neg");
-							}
-							break;
+						const kccDelta = entry.kcCoins - previousEntry.kcCoins;
 
-						case "victories":
-							valueEl.innerHTML = entry.victories;
-							const deltaVictories = entry.victories - (previousEntry.victories ?? entry.victories);
-							if (deltaVictories != 0) {
-								deltaValueEl.innerHTML = `${deltaVictories > 0 ? "+" : ""}${deltaVictories}`;
-								deltaValueEl.classList.add(deltaVictories > 0 ? "pos" : "neg");
-							}
-							break;
+						if (kccDelta > 20) generateFact(`${id} a gagné ${kccDelta} KCC`, "pos");
+						else if (kccDelta < -20) generateFact(`${id} a dépensé ${Math.abs(kccDelta)} KCC`, "neg");
+
+						const victoriesDelta = entry.victories - previousEntry.victories;
+						if (victoriesDelta > 2) generateFact(`${id} a gagné ${victoriesDelta} fois`, "pos");
 					}
 				}
 
-				element.addEventListener("click", async () => {
-					for (const el of container.querySelectorAll(".member")) {
-						if (el != element) el.classList.remove("open");
+				const sortedLeaderboard = sortLeaderboard(leaderboard, category);
+				for (const id in sortedLeaderboard) {
+					const rank = findRankFor(leaderboard, id, category);
+					const previousRank = findRankFor(previousLeaderboard, id, category);
+
+					const entry = leaderboard[id];
+					const previousEntry = previousLeaderboard[id] ?? {};
+
+					const element = document.createElement("div");
+					element.classList.add("member");
+					element.setAttribute("rank", rank);
+
+					{
+						const rankEl = document.createElement("div");
+						rankEl.classList.add("rank");
+						rankEl.innerHTML = formatRank(rank);
+						element.append(rankEl);
+
+						const deltaEl = document.createElement("div");
+						deltaEl.classList.add("delta");
+						const delta = previousRank - rank;
+						if (delta != 0) {
+							deltaEl.innerHTML = `${delta > 0 ? "+" : ""}${delta}`;
+							deltaEl.classList.add(delta > 0 ? "pos" : "neg");
+						}
+						element.append(deltaEl);
+
+						const nameEl = document.createElement("div");
+						nameEl.classList.add("name");
+						nameEl.innerHTML = id;
+						element.append(nameEl);
+
+						const valueEl = document.createElement("div");
+						valueEl.classList.add("value");
+						element.append(valueEl);
+
+						const deltaValueEl = document.createElement("div");
+						deltaValueEl.classList.add("delta-value");
+						element.append(deltaValueEl);
+
+						const detailsEl = document.createElement("div");
+						detailsEl.classList.add("details");
+						element.append(detailsEl);
+
+						switch (category) {
+							case "ratio":
+								valueEl.innerHTML = entry.ratio > 0 ? `${Number((entry.ratio * 100).toFixed(1))}%` : "--";
+								const deltaRatio = entry.ratio > 0 ? entry.ratio - (previousEntry.ratio ?? entry.ratio) : 0;
+								if (deltaRatio != 0) {
+									deltaValueEl.innerHTML = `${deltaRatio > 0 ? "+" : ""}${Number((deltaRatio * 100).toFixed(1))}%`;
+									deltaValueEl.classList.add(deltaRatio > 0 ? "pos" : "neg");
+								}
+								break;
+
+							case "bank":
+								valueEl.innerHTML = entry.kcCoins;
+								const deltaKCC = entry.kcCoins - (previousEntry.kcCoins ?? entry.kcCoins);
+								if (deltaKCC != 0) {
+									deltaValueEl.innerHTML = `${deltaKCC > 0 ? "+" : ""}${deltaKCC}`;
+									deltaValueEl.classList.add(deltaKCC > 0 ? "pos" : "neg");
+								}
+								break;
+
+							case "entries":
+								valueEl.innerHTML = entry.entries;
+								const deltaEntries = entry.entries - (previousEntry.entries ?? entry.entries);
+								if (deltaEntries != 0) {
+									deltaValueEl.innerHTML = `${deltaEntries > 0 ? "+" : ""}${deltaEntries}`;
+									deltaValueEl.classList.add(deltaEntries > 0 ? "pos" : "neg");
+								}
+								break;
+
+							case "victories":
+								valueEl.innerHTML = entry.victories;
+								const deltaVictories = entry.victories - (previousEntry.victories ?? entry.victories);
+								if (deltaVictories != 0) {
+									deltaValueEl.innerHTML = `${deltaVictories > 0 ? "+" : ""}${deltaVictories}`;
+									deltaValueEl.classList.add(deltaVictories > 0 ? "pos" : "neg");
+								}
+								break;
+						}
 					}
 
-					const open = element.classList.toggle("open");
+					element.addEventListener("click", async () => {
+						for (const el of container.querySelectorAll(".member")) {
+							if (el != element) el.classList.remove("open");
+						}
 
-					if (open) await loadDetails(element, id);
-				});
+						const open = element.classList.toggle("open");
 
-				container.append(element);
+						if (open) await loadDetails(element, id);
+					});
+
+					container.append(element);
+				}
 			}
-		}
-	};
+		};
 
-	const params = new URLSearchParams(location.search);
-	let entry = entries.length - 1;
+		const params = new URLSearchParams(location.search);
+		let entry = entries.length - 1;
 
-	const changeCategory = async (category) => {
-		await loadLeaderboard(entry, category);
-		const url = new URL(location.href);
-		params.set("category", category);
-		url.searchParam = params;
-		updateURL(url);
-	};
+		const changeCategory = async (category) => {
+			await loadLeaderboard(entry, category);
+			const url = new URL(location.href);
+			params.set("category", category);
+			url.searchParam = params;
+			updateURL(url);
+		};
 
-	ratioButton.addEventListener("click", async () => await changeCategory("ratio"));
-	bankButton.addEventListener("click", async () => await changeCategory("bank"));
-	entriesButton.addEventListener("click", async () => await changeCategory("entries"));
-	victoriesButton.addEventListener("click", async () => await changeCategory("victories"));
+		ratioButton.addEventListener("click", async () => await changeCategory("ratio"));
+		bankButton.addEventListener("click", async () => await changeCategory("bank"));
+		entriesButton.addEventListener("click", async () => await changeCategory("entries"));
+		victoriesButton.addEventListener("click", async () => await changeCategory("victories"));
 
-	const category = ["ratio", "bank", "entries", "victories"].includes(params.get("category")) ? params.get("category") : "ratio";
-	if (params.get("date")) {
-		const selectedDate = new Date(params.get("date"));
-		selectedDate.setUTCHours(0, 0, 0, 0);
-		date.setAttribute("date", `${selectedDate.getUTCFullYear()}-${String(selectedDate.getUTCMonth() + 1).padStart(2, "0")}-${String(selectedDate.getUTCDate()).padStart(2, "0")}`);
+		const category = ["ratio", "bank", "entries", "victories"].includes(params.get("category")) ? params.get("category") : "ratio";
+		if (params.get("date")) {
+			const selectedDate = new Date(params.get("date"));
+			selectedDate.setUTCHours(0, 0, 0, 0);
+			date.setAttribute("date", `${selectedDate.getUTCFullYear()}-${String(selectedDate.getUTCMonth() + 1).padStart(2, "0")}-${String(selectedDate.getUTCDate()).padStart(2, "0")}`);
 
-		entry = entries.findIndex((value) => `${selectedDate.getUTCFullYear()}-${String(selectedDate.getUTCMonth() + 1).padStart(2, "0")}-${String(selectedDate.getUTCDate()).padStart(2, "0")}` == `${value.getUTCFullYear()}-${String(value.getUTCMonth() + 1).padStart(2, "0")}-${String(value.getUTCDate()).padStart(2, "0")}`) ?? -1;
-		await loadLeaderboard(entry, category);
-	} else await loadLeaderboard(entry, category);
+			entry = entries.findIndex((value) => `${selectedDate.getUTCFullYear()}-${String(selectedDate.getUTCMonth() + 1).padStart(2, "0")}-${String(selectedDate.getUTCDate()).padStart(2, "0")}` == `${value.getUTCFullYear()}-${String(value.getUTCMonth() + 1).padStart(2, "0")}-${String(value.getUTCDate()).padStart(2, "0")}`) ?? -1;
+			await loadLeaderboard(entry, category);
+		} else await loadLeaderboard(entry, category);
+	} else {
+		container.style.display = "none";
+		lastUpdated.classList.remove("loading");
+	}
 });
