ea2333212074fe756ee3abbfc8d1ab1f901a7d01
Wixonic <contact@wixonic.fr>
2026-01-07 20:21:18 +0100

    refactor: Reimplement navigation toggle logic, update header and navigation styles, enhance text and table styling, and update license year.

--- src/lib/mutations/nav.js
+++ src/lib/mutations/nav.js
@@ -1,27 +1,22 @@
 export default {
 	"nav": {
 		added: (node) => {
-			const button = document.createElement("button");
-			button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 256 256"><path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16Zm176,112H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"></path></svg>`;
-
-			button.classList.add("nav-toggle");
+			const button = document.querySelector(".nav-toggle");
+			button.classList.add("active");
 
 			button.addEventListener("click", () => {
-				node.classList.toggle("hidden");
+				node.classList.toggle("visible");
 			});
-
-			const header = document.querySelector("body > header");
-			if (header) header.prepend(button);
 
 			node.querySelectorAll("a").forEach((link) => {
 				link.addEventListener("click", () => {
-					node.classList.add("hidden");
+					node.classList.remove("visible");
 				});
 			});
 		},
 		removed: (node) => {
-			const button = document.querySelector("body > header > .nav-toggle");
-			if (button) button.remove();
+			const button = document.querySelector(".nav-toggle");
+			button.classList.remove("active");
 		}
 	}
 };
