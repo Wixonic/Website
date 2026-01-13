a6d4bb9b237accae0525760099af861a441b5d31
Wixonic <contact@wixonic.fr>
2026-01-13 11:08:09 +0100

    Add cookie translation and slide animation after loading.

--- src/lib/main.js
+++ src/lib/main.js
@@ -116,48 +116,26 @@ const init = async () => {
 
 	if (storage.getItem("consent") === "true") {
 		document.head.append(analyticScript);
-	} else if (!storage.getItem("consent")) {
-		const cookiePopup = document.createElement("aside");
-		cookiePopup.classList.add("cookie");
-
-		cookiePopup.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 256 256">
-	<path d="M164.49,163.51a12,12,0,1,1-17,0A12,12,0,0,1,164.49,163.51Zm-81-8a12,12,0,1,0,17,0A12,12,0,0,0,83.51,155.51Zm9-39a12,12,0,1,0-17,0A12,12,0,0,0,92.49,116.49Zm48-1a12,12,0,1,0,0,17A12,12,0,0,0,140.49,115.51ZM232,128A104,104,0,1,1,128,24a8,8,0,0,1,8,8,40,40,0,0,0,40,40,8,8,0,0,1,8,8,40,40,0,0,0,40,40A8,8,0,0,1,232,128Zm-16.31,7.39A56.13,56.13,0,0,1,168.5,87.5a56.13,56.13,0,0,1-47.89-47.19,88,88,0,1,0,95.08,95.08Z" />
-</svg>`;
-
-		const description = document.createElement("div");
-		description.classList.add("description");
-		description.innerHTML = `Do you want some cookies?<br />We need a few to make the site work (the serious stuff), and some others to track how cool this page is (Analytics). See the recipe and ingredients in our <a href="${new URL("/privacy/", path.root)}" target="_blank">Privacy Policy</a><br />What's your flavor?`;
-
-		const buttons = document.createElement("div");
-		buttons.classList.add("buttons");
-
-		const acceptButton = document.createElement("button");
-		acceptButton.classList.add("primary");
-		acceptButton.innerHTML = "Accept All";
-		acceptButton.addEventListener("click", () => {
-			storage.setItem("consent", "true");
-			document.head.append(analyticScript);
-			cookiePopup.remove();
-		});
-
-		const acceptFeaturesButton = document.createElement("button");
-		acceptFeaturesButton.innerHTML = "Decline Analytics";
-		acceptFeaturesButton.addEventListener("click", () => {
-			storage.setItem("consent", "features");
-			cookiePopup.remove();
-		});
-
-		const declineButton = document.createElement("button");
-		declineButton.innerHTML = "Decline All";
-		declineButton.addEventListener("click", () => {
-			storage.setItem("consent", "false");
-			cookiePopup.remove();
-		});
-
-		buttons.append(acceptButton, acceptFeaturesButton, declineButton);
-		cookiePopup.append(description, buttons);
-		document.body.append(cookiePopup);
 	}
+
+	const cookiePopup = document.querySelector("aside.cookie");
+	if (!storage.getItem("consent")) cookiePopup.classList.remove("hidden");
+
+	cookiePopup.querySelector("#cookie-accept").addEventListener("click", () => {
+		storage.setItem("consent", "true");
+		document.head.append(analyticScript);
+		cookiePopup.classList.add("hidden");
+	});
+
+	cookiePopup.querySelector("#cookie-decline-analytics").addEventListener("click", () => {
+		storage.setItem("consent", "features");
+		cookiePopup.classList.add("hidden");
+	});
+
+	cookiePopup.querySelector("#cookie-decline").addEventListener("click", () => {
+		storage.setItem("consent", "false");
+		cookiePopup.classList.add("hidden");
+	});
 };
 
 export {
