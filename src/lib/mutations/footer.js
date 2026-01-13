e7aef4512c9574a626bd82df2d629517532d2f22
Wixonic <contact@wixonic.fr>
2026-01-13 11:08:07 +0100

    Add basics for footer.

--- src/lib/mutations/footer.js
+++ src/lib/mutations/footer.js
@@ -1,12 +1,9 @@
-import storage from "../script/storage.js";
-
 export default {
 	"footer > .cookie": {
 		added: (node) => {
 			node.addEventListener("click", () => {
-				storage.removeItem("consent");
-				location.reload();
+				document.querySelector("aside.cookie").classList.remove("hidden");
 			});
 		}
 	}
-};
+}
