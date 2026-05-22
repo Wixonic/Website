890f2217d9234c0d6345879ab6f737ac970dc9e5
Wixonic <contact@wixonic.fr>
2026-05-22 13:05:09 +0200

    feat: Update

--- src/script/utils.js
+++ src/script/utils.js
@@ -1,7 +1,22 @@
 /**
  * @param {number} ms 
+ * @returns {string}
+ */
+const parseDuration = (ms) => {
+	const seconds = Math.floor(ms / 1000);
+	const minutes = Math.floor(seconds / 60);
+	const hours = Math.floor(minutes / 60);
+	const days = Math.floor(hours / 24);
+
+	if (days > 0) return `${days} day${days !== 1 ? "s" : ""}`;
+	if (hours > 0) return `${hours} hour${hours !== 1 ? "s" : ""}`;
+	if (minutes > 0) return `${minutes} minute${minutes !== 1 ? "s" : ""}`;
+	return `${seconds} second${seconds !== 1 ? "s" : ""}`;
+};
+
+/**
+ * @param {number} ms 
  * @returns {Promise<void>}
  */
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-export { wait };
+export { parseDuration, wait };
--- websites/root/index.js
+++ websites/root/index.js
@@ -10,12 +10,15 @@ import {
 import logger from "/script/logger.js";
 import { request } from "/script/request.js";
 import { db } from "/script/firebase.js";
+import { parseDuration } from "/script/utils.js";
+
+/** @typedef {""} ButtonType */
 
 /**
  * @typedef {Object} Button
  * @property {string} content
  * @property {string} link
- * @property {string} [type]
+ * @property {ButtonType} [type]
  */
 
 /**
@@ -30,22 +33,20 @@ import { db } from "/script/firebase.js";
  * @property {string} [image]
  */
 
-
+/** @typedef {"Blender" | "Discord" | "GitHub" | "YouTube"} Tag */
 /**
  * @typedef {Object} News
  * @property {string} id
  * @property {string} title
  * @property {string} summary
  * @property {Date} date
+ * @property {Tag} [tag]
  * @property {boolean} pinned
  * @property {Button[]} [buttons]
  * @property {string} [image]
  */
 
-/**
- * @typedef {"GitHub" | "Blender" | "Other"} ProjectType
- */
-
+/** @typedef {"Blender" | "GitHub" | "YouTube"} ProjectType */
 /**
  * @typedef {Object} Project
  * @property {string} id
@@ -55,7 +56,7 @@ import { db } from "/script/firebase.js";
  * @property {boolean} pinned
  * @property {Button[]} [buttons]
  * @property {string} [image]
- * @property {ProjectType[]} [type]
+ * @property {ProjectType} [type]
  */
 
 addEventListener("DOMContentLoaded", async () => {
@@ -92,7 +93,8 @@ addEventListener("DOMContentLoaded", async () => {
 		news.push(...newsSnap.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
 		projects.push(...projectsSnap.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
 
-		const eventContainer = document.querySelector("section.events");
+		const eventContainer = document.querySelector("section.events .content");
+		const eventElements = [];
 		for (const event of events) {
 			const eventElement = document.createElement("article");
 			eventElement.classList.add("event");
@@ -107,11 +109,68 @@ addEventListener("DOMContentLoaded", async () => {
 			summary.textContent = event.summary;
 			eventElement.append(summary);
 
-			eventContainer.append(eventElement);
+			eventElements.push(eventElement);
 		}
+		eventContainer.innerHTML = "";
+		eventContainer.append(...eventElements);
 
-		console.log("News:", news);
-		console.log("Projects:", projects);
+		const newsContainer = document.querySelector("section.news .content");
+		const newsElements = [];
+		for (const newsItem of news) {
+			const newsElement = document.createElement("article");
+			newsElement.classList.add("news");
+			if (newsItem.pinned) newsElement.classList.add("pinned");
+
+			if (newsItem.tag) {
+				const tag = document.createElement("div");
+				tag.classList.add("tag");
+				tag.textContent = newsItem.tag;
+				newsElement.append(tag);
+			}
+
+			const title = document.createElement("h3");
+			title.textContent = newsItem.title;
+			newsElement.append(title);
+
+			const summary = document.createElement("p");
+			summary.classList.add("content");
+			summary.textContent = newsItem.summary;
+			newsElement.append(summary);
+
+			newsElements.push(newsElement);
+		}
+		newsContainer.innerHTML = "";
+		newsContainer.append(...newsElements);
+
+		const projectsContainer = document.querySelector("section.projects .content");
+		const projectsElements = [];
+		for (const project of projects) {
+			const projectElement = document.createElement("article");
+			projectElement.classList.add("project");
+			if (project.pinned) projectElement.classList.add("pinned");
+
+			if (project.type) {
+				const tag = document.createElement("div");
+				tag.classList.add("project");
+				tag.textContent = project.type;
+				projectElement.append(tag);
+
+				if (["Blender", "GitHub", "YouTube"].includes(project.type)) tag.classList.add("external");
+			}
+
+			const title = document.createElement("h3");
+			title.textContent = project.title;
+			projectElement.append(title);
+
+			const summary = document.createElement("p");
+			summary.classList.add("content");
+			summary.textContent = project.summary;
+			projectElement.append(summary);
+
+			projectsElements.push(projectElement);
+		}
+		projectsContainer.innerHTML = "";
+		projectsContainer.append(...projectsElements);
 
 		const githubEventsRequest = await request("GET", `https://api.github.com/users/{{ path.github.username }}/events/public?per_page=3`, "json", null, null, 300);
 		const githubEvents = githubEventsRequest.response;
@@ -121,7 +180,32 @@ addEventListener("DOMContentLoaded", async () => {
 				.filter((event) => event.type === "PushEvent" && event.public === true)
 				.slice(0, 5);
 
-			console.log("Last GitHub commits:", commits);
+			const commitContainer = document.querySelector("section.projects article.commits .content");
+			for (const commit of commits) {
+				console.log(commit);
+				const commitElement = document.createElement("a");
+				commitElement.href = `https://github.com/${commit.repo.name}/commit/${commit.payload.head}`;
+				commitElement.target = "_blank";
+				commitElement.classList.add("commit", "unlink");
+
+				const hashElement = document.createElement("div");
+				hashElement.classList.add("hash");
+				hashElement.textContent = commit.payload.head.substring(0, 7);
+				commitElement.append(hashElement);
+
+				const sourceElement = document.createElement("div");
+				sourceElement.classList.add("source");
+				sourceElement.textContent = commit.repo.name.replace("{{ path.github.username }}/", "");
+				commitElement.append(sourceElement);
+
+				const dateElement = document.createElement("div");
+				dateElement.classList.add("date");
+				const commitDate = new Date(commit.created_at);
+				dateElement.textContent = `${parseDuration(Date.now() - commitDate.getTime())} ago`;
+				commitElement.append(dateElement);
+
+				commitContainer.append(commitElement);
+			}
 		} else logger.warn("GitHub API request failed", `Status: ${githubEventsRequest.status}`);
 	} catch (error) {
 		logger.error("Data import failed", error.message, error.stack);
