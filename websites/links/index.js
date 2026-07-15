22f81af8d891ad91aa75aa0ac578324dbe255f4e
Wixonic <contact@wixonic.fr>
2026-07-15 11:20:04 +0900

    feat: Initial link UI/UX

---
+++ websites/links/index.js
@@ -0,0 +1,35 @@
+addEventListener("DOMContentLoaded", () => {
+	const linkSection = document.querySelector(".links");
+
+	if (linkSection.getBoundingClientRect().width > 1024) {
+		for (const image of linkSection.querySelectorAll("img")) image.src = image.src.replace(".min", "");
+		console.log("Switched to full-size images.");
+	}
+
+	const links = linkSection.querySelectorAll("a");
+	const ratios = new Map();
+
+	const observer = new IntersectionObserver((entries) => {
+		for (const entry of entries) ratios.set(entry.target, entry.intersectionRatio);
+
+		let maxRatio = -1;
+		let activeLink = null;
+
+		for (const [link, ratio] of ratios) {
+			if (ratio > maxRatio) {
+				maxRatio = ratio;
+				activeLink = link;
+			}
+		}
+
+		for (const link of links) {
+			if (link === activeLink && maxRatio > 0) link.classList.add("active");
+			else link.classList.remove("active");
+		}
+	}, {
+		root: linkSection,
+		threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9, 1.0]
+	});
+
+	for (const link of links) observer.observe(link);
+});
