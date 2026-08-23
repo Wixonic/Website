5f678c47781627296225a782f1ec2314c717f856
Wixonic <contact@wixonic.fr>
2026-08-23 12:19:32 +0200

    chore: This was an old duplicate. Replaced by WixiBot version

--- src/modules/error.ts
+++ src/modules/error.ts
@@ -1,14 +1,18 @@
-import type { Handler } from "../../Server/src/main.ts";
+import type { Handler, Logger } from "../../Server/src/main.ts";
 import { config } from "../../Server/src/config.ts";
 
 export const handler: Handler = {
 	domain: config.isDevEnvironment ? "localhost:1200" : "server.wixonic.fr",
 	origin: "*",
 	path: "/error",
-	handle: async (req) => {
-		if (req.method !== "POST") return new Response("Method Not Allowed", { status: 405 });
-		const data = await req.json().catch(() => null);
-		console.error("[Client Error Report]", data);
-		return new Response(null, { status: 204 });
+	handle: async (logger: Logger, request: Request) => {
+		if (request.method !== "POST") return new Response("Method Not Allowed", { status: 405 });
+
+		const data = await request.json().catch(() => null);
+
+		if (data?.location) {
+			logger.error(`A fatal error occured on the website:\n${data.reason}\n${data.message}\n${data.trace}\n-# Location: <${data.location}>`);
+			return new Response(null, { status: 200 });
+		} else return new Response(null, { status: 400 });
 	}
 };
