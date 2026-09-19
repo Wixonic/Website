f7cf3ba267495890580729bb18740471d0b562aa
Wixonic <contact@wixonic.fr>
2026-09-19 14:51:51 +0200

    feat: Update about page

--- websites/root/about/index.js
+++ websites/root/about/index.js
@@ -7,26 +7,24 @@ const TMDB = (path = "/") => request("GET", `${TMDB_BASE}${path}?api_key=${TMDB_
 
 const carousels = {
 	movies: [
-		10681, // Wall-E
-		522402, // Finch
+		10681,   // Wall-E
+		522402,  // Finch
 		1184918, // A Wild Robot
-		920, // Cars
-		2062 // Ratatouille
+		920,     // Cars
+		2062,    // Ratatouille
+		77338,   // Intouchables
+		516486   // Greyhound
 	],
 	shows: [
-		95480, // Slow Horses
-		95396, // Severance
-		97546, // Ted Lasso
+		95480,  // Slow Horses
+		231001, // Carême
+		95396,  // Severance
+		97546,  // Ted Lasso
 		125988, // Silo
 		252107, // Star City
 		157368, // The New Look
-		93740, // Foundation
-		225171, // Pluribus
-		46518, // Masters of the Air
-		278624, // Lucky
-		87917, // For All Mankind
-		252000, // Down Cemetery Road
-		157226 // Sunny
+		93740,  // Foundation
+		225171  // Pluribus
 	],
 	games: [
 
@@ -55,65 +53,74 @@ addEventListener("DOMContentLoaded", async () => {
 			const TMDB_IMAGES_BASE = configRequest.response?.images?.secure_base_url;
 			const TMDB_IMAGE = (path = "/", file_size = "original") => `${TMDB_IMAGES_BASE}/${file_size}${path}?api_key=${TMDB_KEY}`;
 
-			try {
-				for (const id of carousels.shows) {
-					const req = await TMDB(`/tv/${id}`);
+			await Promise.all([
+				new Promise(async (resolve) => {
+					try {
+						for (const id of carousels.shows) {
+							const req = await TMDB(`/tv/${id}`);
 
-					if (req.status == 200) {
-						const data = req.response;
+							if (req.status == 200) {
+								const data = req.response;
 
-						const element = document.createElement("a");
-						element.classList.add("element", "show", "unlink");
-						element.href = `https://www.themoviedb.org/tv/${id}`;
-						element.target = "_blank";
-						element.rel = "noopener noreferrer";
+								const element = document.createElement("a");
+								element.classList.add("element", "show", "unlink");
+								element.href = `https://www.themoviedb.org/tv/${id}`;
+								element.target = "_blank";
+								element.rel = "noopener noreferrer";
 
-						const title = document.createElement("div");
-						title.classList.add("title");
-						title.innerHTML = data.original_name;
+								const title = document.createElement("div");
+								title.classList.add("title");
+								title.innerHTML = data.original_name;
 
-						const poster = document.createElement("img");
-						poster.classList.add("poster");
-						poster.src = TMDB_IMAGE(data.poster_path);
+								const poster = document.createElement("img");
+								poster.classList.add("poster");
+								poster.src = TMDB_IMAGE(data.poster_path);
 
-						element.append(title, poster);
+								element.append(title, poster);
 
-						carouselElements.shows.append(element);
+								carouselElements.shows.append(element);
+
+								resolve();
+							}
+						}
+					} catch (error) {
+						carouselElements.shows.innerHTML = "Failed to load TV Shows.";
 					}
-				}
-			} catch (error) {
-				carouselElements.shows.innerHTML = "Failed to load TV Shows.";
-			}
+				}),
+				new Promise(async (resolve) => {
+					try {
+						for (const id of carousels.movies) {
+							const req = await TMDB(`/movie/${id}`);
 
-			try {
-				for (const id of carousels.movies) {
-					const req = await TMDB(`/movie/${id}`);
+							if (req.status == 200) {
+								const data = req.response;
 
-					if (req.status == 200) {
-						const data = req.response;
+								const element = document.createElement("a");
+								element.classList.add("element", "movie", "unlink");
+								element.href = `https://www.themoviedb.org/movie/${id}`;
+								element.target = "_blank";
+								element.rel = "noopener noreferrer";
 
-						const element = document.createElement("a");
-						element.classList.add("element", "movie", "unlink");
-						element.href = `https://www.themoviedb.org/movie/${id}`;
-						element.target = "_blank";
-						element.rel = "noopener noreferrer";
+								const title = document.createElement("div");
+								title.classList.add("title");
+								title.innerHTML = data.original_title;
 
-						const title = document.createElement("div");
-						title.classList.add("title");
-						title.innerHTML = data.original_title;
+								const poster = document.createElement("img");
+								poster.classList.add("poster");
+								poster.src = TMDB_IMAGE(data.poster_path);
 
-						const poster = document.createElement("img");
-						poster.classList.add("poster");
-						poster.src = TMDB_IMAGE(data.poster_path);
+								element.append(title, poster);
 
-						element.append(title, poster);
+								carouselElements.movies.append(element);
 
-						carouselElements.movies.append(element);
+								resolve();
+							}
+						}
+					} catch (error) {
+						carouselElements.movies.innerHTML = "Failed to load movies.";
 					}
-				}
-			} catch (error) {
-				carouselElements.movies.innerHTML = "Failed to load movies.";
-			}
+				})
+			]);
 		}
 	} catch (error) {
 		carouselElements.shows.innerHTML = "Failed to load TV Shows.";
